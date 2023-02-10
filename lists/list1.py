3f54200d86838fbfe1a5f9c6d1c0df45b7724b8a
carlmizzy <mwauracal18@gmail.com>
2023-02-09 23:53:49 -0800

    python lists

---
+++ lists/list1.py
@@ -0,0 +1,47 @@
+# my first python list
+numbers = [45, 87, 100, 34, 78, 23, 17]
+
+print(len(numbers)) # get the length on my list
+
+# for i in range(len(numbers)):
+#     print(numbers[i])
+#LIST METHODS
+# Index
+print(numbers.index(87)
+
+#append -adds an element to the end of a 
+
+# before appending
+for i in range(len(numbers));
+    print(numbers[i])
+#print(name)
+
+#sort
+#count
+#pop
+#insert
+numbers.insert(2, 100)
+for i in range(len(numbers)):
+    print(numbers[i])
+#tips and tricks
+#multiplying a list
+fives =[5]* 5
+fives
+#slicing alist
+#creating a list
+nums=[] #empty list
+for i in range(10):
+   #insert elements into the list
+   nums.append(i)
+
+for i in range(len(nums)):
+    print(nums[i])
+
+even_nums=[]  
+for num in range(20):
+    if (num % 2==0):
+        nums.append(num)
+for i in range (len(nums)):
+    print(nums[i])
+
+
